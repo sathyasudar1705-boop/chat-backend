6b1b6f35a9d9a53e2b8ef3f82d89882d426958bc
Sathyasudar Vetri Vasagam <sathyasudarv@systechusa.com>
2026-06-18 17:22:24 +0530

    robust path resolution in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,9 @@
-from main import app
+import os
+import sys
+
+# Ensure the root directory is at the top of the Python import search path
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+
+from backend.main import app
